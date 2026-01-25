abb2fcbc4ca3745a0c0c6a3680c74e527f5a5ecf
kadyrzhanov <kadyrzhanov@itsource.kg>
2026-01-26 02:53:28 +0600

    a

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,9 @@ export default defineConfig({
     },
   },
   server: {
-    host: true,          // 0.0.0.0
+    host: true,         
     port: 5173,
-    strictPort: true,    // не перескакивать на другой порт
+    strictPort: true,    
   },
   preview: {
     port: 5173,
